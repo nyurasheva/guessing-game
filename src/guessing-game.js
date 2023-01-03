33f8b0695e90e37aeac8f7d9d1a2df9a00bd2d03
Анастасия Юрашева <anastasiauraseva@air-anastasia.local>
2023-01-03 15:51:24 +0300

    feat: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.left = min
+        this.right = max
     }
 
     guess() {
-
+        this.mid = Math.round((this.right - this.left) / 2) + this.left
+        return this.mid
     }
 
     lower() {
-
+        this.right = this.mid
     }
 
     greater() {
-
+        this.left = this.mid
     }
 }
 
